acb7d9bcd50915649904805dc6b0bda4b3cc1b1a
Howard Zhou <howard.g.zhou@medtronic.com>
2018-10-12 12:48:49 -0400

    updated

--- 1_pointers.c
+++ 1_pointers.c
@@ -14,9 +14,14 @@
 	of the last occurrence of that character in the string. Return -1 if the string 
 	does not contain the character.
 
-* -Given two pointers str1 and str2 to null-terminating strings, find the first differing character. If the differing character is smaller in str1 than str2 then return -1. Otherwise return 1. If the strings are equal then return 0. Smaller in this context literally means less than.
-* -Given two pointers dest and src where src is a null-terminating string, copy the contents of src into dest.
-* -Given two pointers dest and src to null-terminating strings, append the contents of src to the end of dest.
+*4 -Given two pointers str1 and str2 to null-terminating strings, find the first 
+	differing character. If the differing character is smaller in str1 than str2 
+	then return -1. Otherwise return 1. If the strings are equal then return 0. 
+	Smaller in this context literally means less than.
+
+*5 -Given two pointers dest and src where src is a null-terminating string, copy the contents of src into dest.
+*
+ 6-Given two pointers dest and src to null-terminating strings, append the contents of src to the end of dest.
 * You won't need malloc to write these functions, though it would be useful to test them. Let me know if you have any questions.
 
 strings can be stored either
@@ -29,10 +34,13 @@ char *str2 = "Goodbye"
 #include <stdio.h>
 #include <stdlib.h>
 
+
+/*
+* -Given a pointer to a null-terminating string, find the length of the string. - DONE
+*/
 //Const pointer will not hcange what is pointed to)
 int str_len(const char* s) {
 	int string_length;
-	char * temp;
 	// Array method
 	for (int i=0; s[i] != '\0'; i++) {
 		printf("s[%d] = %c\n", i,s[i] );
@@ -46,17 +54,25 @@ int str_len(const char* s) {
 	return string_length;
 }
 
+
+
+/*
+*-Given a pointer to a null-terminating string and a character, find the index 
+* of the first occurrence of that character in the string. Return -1 if the 
+* string does not contain the character. -- DONE
+* 	strings can be stored either
+* * stores &
+* ex: 
+* char * str - pointer variables 
+* str=&str1
+* char str1[] = "Hello"
+* char *str2 = "Goodbye"
+*/
 int str_find(const char* c, const char* s) {
 
-/*	strings can be stored either
-* stores &
-ex: 
-char * str - pointer variables 
-str=&str1
-char str1[] = "Hello"
-char *str2 = "Goodbye"
-*/
-int index_pos=-1;
+
+
+	int index_pos=-1;
 	int i =0;
 	printf("comparision char: %c\n", *c);
 
@@ -64,8 +80,13 @@ int index_pos=-1;
 		printf("s[%d] = %c\n", i, s[i]);
 		// printf("%c\n", *s+i); //why won't work?
 		i++;
+
+		if (s[i] == '\0'){
+			i=-1;
+		}
 	
-	} while( s[i] != *c);
+	} while( s[i] != *c && s[i] !='\0');
+	
 	printf("s[%d] --> %c\n", i, s[i]);
 	index_pos = i;
 
@@ -73,12 +94,82 @@ int index_pos=-1;
 }
 
 
+
+
+/*
+* Given a pointer to a null-terminating string and a character, find the index 
+* of the last occurrence of that character in the string. Return -1 if the string 
+* does not contain the character.
+*/
+int last_occur(const char* c, const char* s) {
+	//iterati
+	int index_pos = -1;
+	int loop_on = 1;
+	int i=0;
+
+	do {
+		printf("s[%d] = %c\n", i, s[i]);
+
+		if (s[i] == *c) {
+			index_pos = i;
+		}
+		else if (s[i] == '\0'){
+			loop_on = 0;
+		}
+		i++;
+	} while (loop_on == 1);
+
+	return index_pos;
+}
+
+/**4 -Given two pointers str1 and str2 to null-terminating strings, find the first 
+	differing character. If the differing character is smaller in str1 than str2 
+	then return -1. Otherwise return 1. If the strings are equal then return 0. 
+	Smaller in this context literally means less than.
+	insane inssanity
+*/
+int diff_str(const char* s1, const char* s2 ){
+	int loop_on=1;
+	int match_found = 0;
+	int j=0;
+	int index=0;
+
+	//algorithm is good. use do while to fix.
+	for (int i=0; match_found == 0 &&  s1[i] != '\0'; i++){
+		// iterate through second s2 string
+		do {
+			printf("s1[%d]=%c  s2[%d]=%c\n", i, s1[i], j,s2[j]);
+			j++;
+
+		} while (s1[i] != s2[j] && s2[j] != '\0');
+		
+		if (s1[i] == s2[j]) {
+			printf("match found s1[%d]=%c s2[%d]=%c\n", i,s1[i],j,s2[j] );
+			match_found = 1;
+			index=i;
+		}
+	
+	}
+
+
+	return s1[index] == s2[j] ? 1: -1;
+}
+
+/* Main driver class */
 int main (int argc, char**argv) {
 	// int l = str_len("Barad"); // goes to literal memory
 	// printf ("l = %d\n", l);
 
-	int sf=str_find("a", "insane");
-	printf("str_find index pos: %d\n", sf);
+	// int sf=str_find("a", "insanaf");
+	// printf("str_find index pos: %d\n", sf);
+
+	// int loccur=last_occur("a", "insanafas");
+	// printf("last occur pos: %d\n", loccur);
+
+	int test=diff_str("once", "bounce");
+
+	printf("test: %d", test );
+
 	return 0;
 }
 
