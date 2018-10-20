926a9e1a5751f654a9a8d0f4b5583eb690517e1d
hzhou849 <hzhou849>
2018-10-20 13:40:19 -0400

    queue update

---
+++ queue.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct queue {
+	short* start;
+	int capacity;
+	short* eq;
+	short* dq;
+}
+
+
+int queue_init( queue *q, int capacity) {
+	if (q == 0 ) {
+
+	}
+}
+int main() {
+
+}
