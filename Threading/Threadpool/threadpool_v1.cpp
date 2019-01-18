af54d602b8266c501c2adfbf667ba01ce5d06f71
Howard Zhou <howard.g.zhou@medtronic.com>
2019-01-18 17:42:24 -0500

    added thread pool

--- Threading/Threadpool/threadpool_v1.cpp
+++ Threading/Threadpool/threadpool_v1.cpp
@@ -58,17 +58,21 @@ void Application::loadData () {
 	load_flag = true;
 
 	//notify the condition variables
-	cond_var.notify_one();
+	cond_var.notify_all();
 }
 
 
 void Application::gen_thread(std::function <void(int) > ptr_func) {  // note this 'ptr' only takes one 1 parameter
 	
 	while(!exit_flag) {
-
+		// give another thread a chance, notice this placement must be at the top not bottom of this code block
+		// bc when the while loop resets, it immediately reacquires the lock before the next thread can get it.
+		// this sleep allows the other thread to get the thread in a race.
+		std::this_thread::sleep_for(std::chrono::milliseconds(100)); 
 		std::unique_lock<std::mutex>u_lock(mtx);
 		cond_var.wait(u_lock, std::bind(&Application::get_flag, this));
 		ptr_func(999); // task function()
+		// u_lock.unlock();
 		cond_var.notify_all();		
 	}
 
@@ -82,7 +86,19 @@ void Application::set_exit_flag() {exit_flag = true;}
 
 
 void task(int thread_num, int data) {
+	// for (int i=0; i<2; i++) {
+	// 	std::cout << "Processing task.. Thread:" << thread_num << " B:"<< data << std::endl;
+	// }
 	std::cout << "Processing task.. Thread:" << thread_num << " B:"<< data << std::endl;
+
+	// staggered sleep time to simulate different processing times.
+	if (thread_num == 1) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(500));
+	} else if (thread_num == 2) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
+	} else {
+		std::this_thread::sleep_for(std::chrono::milliseconds(300));
+	}
 }
 
 
@@ -109,29 +125,33 @@ int main() {
 	// }
 
 	// Test case 3:
-
+	// First thread to acquire the mutex will process the task in this thread race.
 	//As you can see, the function we give to callit only accepts one parameter. 
 	//But we want to give it a function that has 2 parameters. So we use bind() to say 
 	//"use whatever paramter callit provides as the 1st param, and use 20 as the 2nd param".
 	//use std::placeholders_<value> if you don't want the pass the actual arg values now.
 
-	std::thread thread1(&Application::gen_thread, &a_obj, std::bind(task,1 , std::placeholders::_1 )); // create the thread
+	std::thread thread1(&Application::gen_thread, &a_obj, std::bind(task, 1, std::placeholders::_1 )); // create the thread
 	std::thread thread2(&Application::gen_thread, &a_obj, std::bind(task, 2, std::placeholders::_1)); 
+	std::thread thread3(&Application::gen_thread, &a_obj, std::bind(task, 3, std::placeholders::_1)); 
+
 	// std::thread thread2(&Application::gen_thread, &a_obj, std::bind(task, std::placeholders::_1, std::placeholders::_2));
 	// std::thread thread1(&Application::gen_thread, &a_obj, std::bind(task, 99,56)); // if you want to pass parameters yourself. recving function <void()>funct
 
-	std::thread thread3 (&Application:: loadData, &a_obj);
+	// std::thread thread3 (&Application:: loadData, &a_obj);
+	a_obj.loadData();
 
-	for (int i=0; i < 1000; i++) {
+
+	for (int i=0; i < 100; i++) {
 		std::cout << "main thread running" << std::endl;
+		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
-
-	std::this_thread::sleep_for(std::chrono::milliseconds(0));
 
 
 	// Cleanup signal flag to close
 	a_obj.set_exit_flag(); 
 	thread1.join();
+	std::cout << "thread1.join()" <<std::endl;
 	thread2.join();
 	thread3.join();
 	
